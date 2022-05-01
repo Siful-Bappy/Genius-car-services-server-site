65af90587f1405efcfe5163f41931d80008bb3dc
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-01 23:42:01 +0900

    got banckend data by using _id

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
-const express = require('express')
-const cors = require("cors");
-require("dotenv").config;
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const res = require('express/lib/response');
+
+const app = express();
 
 // midleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.h2qxn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+      await client.connect();
+      const serviceCollection = client.db("geniusCar").collection("service");
+
+      app.get("/service", async(req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      });
+
+      app.get("/service/:id", async(req, res) => {
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const service = serviceCollection.findOne(query);
+          res.send(service);
+      }) 
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Server is working')
