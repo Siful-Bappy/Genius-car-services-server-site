f8dc30ac9a8b0b4b5819f6a93ba741c64a2d5451
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-05-05 17:00:57 +0900

    start using json web token

--- index.js
+++ index.js
@@ -49,11 +49,20 @@ async function run() {
     })
 
     // order collection api
-    app.post("/order", async(req, res) => {
+    app.get("/order", async(req, res) => {
+      const email = req.query.email;
+      // console.log(email);
+      const query = {email: email};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    })
+
+    app.post('/order', async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
       res.send(result);
-    })
+  })
 
     }
     finally {
